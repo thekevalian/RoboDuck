5194dee63d1f391ea1da81288ffe68c4d9457ea0
Vivek Keval <vivek.keval@utexas.edu>
2023-10-20 13:17:57 -0500

    Dear ImGui has been integrated with SFML and an example from imgui-sfml is available. Makefile needs improvement

--- Software/Application_SW/src/main.cpp
+++ Software/Application_SW/src/main.cpp
@@ -1,52 +1,46 @@
-#include <SFML/Graphics.hpp>
+#include "imgui.h" // necessary for ImGui::*, imgui-SFML.h doesn't include imgui.h
 
-int main( int argc, char** argv )
-{
-	sf::Window window(sf::VideoMode(640, 400), "My First Game", sf::Style::Titlebar|sf::Style::Close);	
-	sf::Event ev;
+#include "imgui-SFML.h" // for ImGui::SFML::* functions and SFML-specific overloads
 
-	// Game Loop
-	while(window.isOpen()){
-		//Event Loop
-		while (window.pollEvent(ev))
-		{
-			switch (ev.type)
-			{
-			case sf::Event::Closed:
-				window.close();
-				break;
-			
-			case sf::Event::KeyPressed:
-				if(ev.key.code == sf::Keyboard::Escape) window.close();
-				break;
-			}
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+// Example from sfml imgui
+int main() {
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    if (!ImGui::SFML::Init(window)) return -1;
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+	bool click_detected = false;
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(window, event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Hello, world!");
+        click_detected = click_detected || ImGui::Button("Look at this pretty button");
+		if(click_detected){
+			ImGui::Text("YOU CLICKED IT");
 		}
-		// Update
+        ImGui::End();
 
-		//Render
-		window.display();
-	}
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
 
-	return 0;
+    ImGui::SFML::Shutdown();
 }
-
-
-
-/*
-Old Stuff
-sf::RenderWindow appWindow( sf::VideoMode( 800, 600, 32 ), "App" );
-	sf::Event appEvent;
-	
-	while ( appWindow.isOpen() )
-	{
-		while ( appWindow.pollEvent( appEvent ) )
-		{
-			if ( appEvent.type == sf::Event::Closed )
-				appWindow.close();
-		}
-
-		appWindow.clear();
-		appWindow.display();
-	}
-
-*/
--- Software/Embedded_SW/RoboDuck/src/main.cpp
+++ Software/Embedded_SW/RoboDuck/src/main.cpp
@@ -11,35 +11,99 @@
 #define MOTOR_APWM 16
 #define MOTOR_BPWM 17
 
+// const char* ssid = "HowWasYourDayToday";
+// const char* password = "9563987865";
+// const char* serverIp = "192.168.50.156";
+// const int serverPort = 8080;
+String cmd;
+
 Motor left(MOTOR_AI1, MOTOR_AI2, MOTOR_APWM);
 Motor right(MOTOR_BI1, MOTOR_BI2, MOTOR_BPWM);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Setting up Motors");
-  if(left.init() && right.init())Serial.println("Succesful");
+  // WiFi.begin(ssid, password);
   
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(1000);
+  //   Serial.println("Connecting to WiFi...");
+  // }
+  pinMode(BUILTIN_LED, OUTPUT);
+  Serial.println("Connected to WiFi");
+  Serial.println("Setting up Motors");
+  if(left.init() && right.init())Serial.println("Successful");
+  left.setSpeed(250);
+  right.setSpeed(-250);
 }
-
+int seconds = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial.available()){
-    String cmd = Serial.readString();
-    if(cmd[0] == 'l'){
-      int speedUpdate = atoi(cmd.c_str()+2);
-      if(left.setSpeed(speedUpdate)){
-        Serial.printf("Speed is now %d\n", speedUpdate);
-      }else{
-        Serial.println("Invalid Value");
-      }
-    }else{
-      int speedUpdate = atoi(cmd.c_str()+2);
-      if(right.setSpeed(speedUpdate)){
-        Serial.printf("Speed is now %d\n", speedUpdate);
-      }else{
-        Serial.println("Invalid Value");
-      }
-    }
+  delay(1000);
+  seconds++;
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  seconds++;
+  digitalWrite(LED_BUILTIN, HIGH);
+  if(seconds % 60 == 0){
+    left.setSpeed(250);
+    right.setSpeed(-250);
   }
+  // put your main code here, to run repeatedly:
+  // if(Serial.available()){
+  //   cmd = Serial.readString();
+  //   if(cmd[0] == 'l'){
+  //     int speedUpdate = atoi(cmd.c_str()+2);
+  //     if(left.setSpeed(speedUpdate)){
+  //       Serial.printf("Speed is now %d\n", speedUpdate);
+  //     }else{
+  //       Serial.println("Invalid Value");
+  //     }
+  //   }else{
+  //     int speedUpdate = atoi(cmd.c_str()+2);
+  //     if(right.setSpeed(speedUpdate)){
+  //       Serial.printf("Speed is now %d\n", speedUpdate);
+  //     }else{
+  //       Serial.println("Invalid Value");
+  //     }
+  //   }
+  // }
+  // if (WiFi.status() == WL_CONNECTED) {
+  //   WiFiClient client;
+    
+  //   if (client.connect(serverIp, serverPort)) {
+  //     // Send data to the server
+  //     client.print("Hello from ESP32!");
+  //     delay(10);
+
+  //     // Receive a response from the server
+  //     while(client.available()){
+  //       cmd = (client.readString());
+  //       Serial.println(cmd);
+  //       Serial.printf("Recieve Length: %d\n", cmd.length());
+  //       if(cmd[0] == 'l' && cmd.length()>0){
+  //         int speedUpdate = atoi(cmd.c_str()+2);
+  //         if(left.setSpeed(speedUpdate)){
+  //           client.printf("Speed is now %d\n", speedUpdate);
+  //         }else{
+  //           client.println("Invalid Value");
+  //         }
+  //       }else if(cmd.length()>0){
+  //         int speedUpdate = atoi(cmd.c_str()+2);
+  //         if(right.setSpeed(speedUpdate)){
+  //           client.printf("Speed is now %d\n", speedUpdate);
+  //         }else{
+  //           client.println("Invalid Value");
+  //         }
+  //   }
+  //     }
+     
+  //     client.stop();
+  //   } else {
+  //     Serial.println("Connection failed");
+  //   }
+    
+  //   delay(3000);  // Delay before making the next connection
+  // }
+
+
 }
